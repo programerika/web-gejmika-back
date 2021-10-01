c7c68efb5dd72a40722578d29e6f4644aaf7340e
Branko Saponjic <branko.saponjic@programerika.com>
2021-10-01 15:44:23 +0200

    Refactoring controller class

--- src/main/java/webgejmikaback/Controller/PlayerScoresController.java
+++ src/main/java/webgejmikaback/Controller/PlayerScoresController.java
@@ -1,6 +1,7 @@
 package webgejmikaback.Controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -25,13 +26,17 @@ public class PlayerScoresController {
 
     @Operation(summary = "Delete score by uid", description = "It deletes player's score from all-scores collection.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "Scores are deleted",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))}),
-            @ApiResponse(responseCode = "400",
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "No content",
+                    content = { @Content(schema = @Schema(implementation = Void.class))}),
+//                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))}),
+            @ApiResponse(
+                    responseCode = "400",
                     description = "Bad request",
                     content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))})
     })
+    // path variabla i response 204
     @RequestMapping(value = "player-scores/{uid}", method = RequestMethod.DELETE)
     public String delete(@PathVariable(name = "username") String username) {
         playerService.delete(username);
@@ -40,24 +45,29 @@ public class PlayerScoresController {
 
     @Operation(summary = "Get player by username", description = "Provide username to get player with score from all-scores collection.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "Player with score",
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success",
                     content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))}),
-            @ApiResponse(responseCode = "400",
+            @ApiResponse(
+                    responseCode = "400",
                     description = "Bad request",
                     content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))})
     })
     @RequestMapping(value = "player-scores/{username}", method = RequestMethod.GET)
-    public Optional<Player> getPlayerByUserName(@RequestParam(name = "username") String username) {
+    public Optional<Player> getPlayerByUserName(@PathVariable(name = "username") String username) {
         return playerService.getPlayerByUsername(username);
     }
 
-    @Operation(summary = "Save player score", description = "Provide username and score to save the new player or update the existing one")
+    @Operation(summary = "Save player score", description = "Provide username and score to save the new player's score")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201",
-                    description = "New score is saved",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))}),
-            @ApiResponse(responseCode = "400",
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Created",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))},
+                    headers = {@Header(name = "Location")}),
+            @ApiResponse(
+                    responseCode = "400",
                     description = "Bad request",
                     content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))})
     })
@@ -66,26 +76,32 @@ public class PlayerScoresController {
         playerService.saveScore(player);
     }
 
-    @Operation(summary = "Add player score", description = "Provide username and score to save the new player or update the existing one")
+    @Operation(summary = "Add player score", description = "Provide username and new score to add score to existing one")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204",
-                    description = "New score is saved",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))}),
-            @ApiResponse(responseCode = "400",
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "No content",
+                    content = { @Content(schema = @Schema(implementation = Void.class))}),
+//                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))}),
+            @ApiResponse(
+                    responseCode = "400",
                     description = "Bad request",
                     content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))})
     })
+    // ubaciti path variablu za username
     @RequestMapping(value = "player-scores/{username}/add-score", method = RequestMethod.POST)
     public void addScore(@RequestBody Player player) {
 
     }
 
-    @Operation(summary = "Top score players", description = "Method gets 10/20 or more top score players")
+    @Operation(summary = "Top score players", description = "Method gets 10 or more top score players")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "Top ten ranked players",
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success",
                     content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))}),
-            @ApiResponse(responseCode = "400",
+            @ApiResponse(
+                    responseCode = "400",
                     description = "Bad request",
                     content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Player.class))})
     })
