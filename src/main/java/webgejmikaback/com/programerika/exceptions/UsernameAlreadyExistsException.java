550b566271ad84b5091d9b201f65f7a53cdaf061
Branko Saponjic <branko.saponjic@programerika.com>
2021-10-17 20:41:03 +0200

    Removed hard coded message from exception

--- src/main/java/webgejmikaback/com/programerika/exceptions/UsernameAlreadyExistsException.java
+++ src/main/java/webgejmikaback/com/programerika/exceptions/UsernameAlreadyExistsException.java
@@ -5,6 +5,6 @@ public class UsernameAlreadyExistsException extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
     public UsernameAlreadyExistsException(String message) {
-        super("Username Already Exists in the Repository or input is not correct");
+        super(message);
     }
 }
