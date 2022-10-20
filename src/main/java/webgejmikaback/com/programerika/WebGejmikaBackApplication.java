ce17b20a18239e2a45533243c174631074c243b9
Branko Saponjic <branko.saponjic@programerika.com>
2022-10-20 09:49:47 +0200

    Added OpenApiDefinition and info

--- src/main/java/webgejmikaback/com/programerika/WebGejmikaBackApplication.java
+++ src/main/java/webgejmikaback/com/programerika/WebGejmikaBackApplication.java
@@ -1,9 +1,16 @@
 package webgejmikaback.com.programerika;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+                                    title = "Gejmika API",
+                                    version = "1.1",
+                                    description = "Backend service for players with multiple games"
+                                ))
 public class WebGejmikaBackApplication{
 	public static void main(String[] args) {
 		// SWAGGER LINK http://localhost:8080/swagger-ui/index.html?configUrl=/api-docs/swagger-config
