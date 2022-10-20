e0d817971b5866f6134d33351cec5d316ce8fb5f
Branko Saponjic <branko.saponjic@programerika.com>
2022-10-20 09:48:04 +0200

    Renamed document collection into "scores"

--- src/main/java/webgejmikaback/com/programerika/model/PlayerScore.java
+++ src/main/java/webgejmikaback/com/programerika/model/PlayerScore.java
@@ -16,7 +16,7 @@ import java.util.Map;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "scoresTestWithMap")
+@Document(collection = "scores")
 public class PlayerScore {
     @Id
     @JsonIgnore
